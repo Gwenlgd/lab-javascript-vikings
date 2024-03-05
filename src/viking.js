8bf61a30a473c6711cb34f916d8f5ef65e37d1a2
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-03-05 11:59:50 +0100

    Almost done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength)
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+
+    // if (damage > 0) {
+    //   return `${this.name} has received ${damage} points of damage`
+    // } else if (damage === 0) {
+    //   return `${this.name} has died in act of combat`
+    // }
+
+    if (damage === 0) {
+      return `${this.name} has died in act of combat`
+    } else if (damage > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength)
+  }
+}
 
 // War
-class War {}
+class War {
+
+}
